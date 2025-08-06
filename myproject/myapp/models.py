88aa3b9ea56ddc895db0c99f54b83af4f52599b6
Aditya Jagtap <aditya.jagtap@indionetworks.com>
2025-08-06 10:07:33 +0530

    starting with django

---
+++ myproject/myapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    grade = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
